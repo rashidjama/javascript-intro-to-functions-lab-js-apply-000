1c0ac384fa47d1ef7babb44516cebb4a1a9417b0
Rashid Jama <rashidjama5@gmail.com>
2019-12-10 02:05:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } if (string.toUpperCase() === string) {
     return "YES INDEED";
-  } if (string === "")
+  } else {
+    return "I love you, too."
+  }
 }
 
