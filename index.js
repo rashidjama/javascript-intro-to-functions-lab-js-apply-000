05b677641f5d3cdcdf2684ee528316f3164a124e
Rashid Jama <rashidjama5@gmail.com>
2019-12-10 02:03:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string.toLowerCase() === string) {
     return "I can't hear you!"
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED";
+  } else {
+    return 
   }
 }
-function sayHiToGrandma(string) {
-  if(string.toUpperCase() === string) {
-    return "YES INDEED!"
-  }
-}
+
